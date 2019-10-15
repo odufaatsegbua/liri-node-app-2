58251ef2e2c6aac6f6cb45ae66cb3e4d70d4a2fc
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-10-15 12:28:10 -0500

    spotify function stopped working, idk why. Getting the overall gist, just need more guidance

--- liri.js
+++ liri.js
@@ -2,29 +2,54 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
- 
-var spotify = new Spotify({
 
-    
+var spotify = new Spotify({
     id: keys.spotify.id,
     secret: keys.spotify.secret,
-  });
+});
 
-  spotify.search({ type: 'track', query: 'Hotline Bling' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data.tracks.items); 
-  });
+var actionCommand = process.argv[2];
+var commandResult = process.argv[3];
+
+switch (actionCommand) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThis()
+        break;
+
+    case "movie-this":
+        movieThis()
+        break;
+
+    case "do-what-it-says":
+        whatever();
+        break;
+}
+
+
+
+function spotifyThis() {
+    console.log("spotify")
+    spotify.search({ type: 'track', query: commandResult }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].name);
+        // console.log(data.tracks.items[0].album.name);
+        // console.log(data.tracks.items[0].artist[0].name);
+        // console.log(data.tracks.items[0].preview_url);  
+    });
+
+}
 
 
 
 
 
 
-// var actionCommand = process.argv[2];
-// var commandResult = process.argv[3];
 
 
 
@@ -32,52 +57,6 @@ var spotify = new Spotify({
 
 
 
-// if (firstCommand === "total") {
-//     fs.readFile("bank.txt", "utf8", function (error, data) {
-//         if (error) {
-//             return console.log(error)
-//         }
 
-//         var entries = data.split(",")
-//         // console.log(entries)
-//         var total = 0;
-//         for (let i = 0; i < entries.length; i++) {
-//             const entry = entries[i];
-//          total = total + parseFloat(entry)
-//         }
 
-        
-//         console.log(total) 
-//     });
-// } else if (firstCommand === "deposit") {
-//     fs.appendFile("bank.txt", ","+returnAmount, function(err) {
-
-//         // If an error was experienced we will log it.
-//         if (err) {
-//           console.log(err);
-//         }
-      
-//         // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-//         else {
-//           console.log("succesful deposit");
-//         }
-      
-//       });
-        
-// } else if (firstCommand === "withdraw") {
-//     fs.appendFile("bank.txt", ","+-returnAmount, function(err) {
-
-//         // If an error was experienced we will log it.
-//         if (err) {
-//           console.log(err);
-//         }
-      
-//         // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-//         else {
-//           console.log("succesful withdraw");
-//         }
-      
-//       });
-        
-// }
-
+// 
