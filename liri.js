60bb525e53a36aa56ea91cd03266b917a7320c6e
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-10-16 08:34:54 -0500

    saving changes from last night, everything is mostly working and complete

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+var axios = require('axios');
+ 
 
 var spotify = new Spotify({
     id: keys.spotify.id,
@@ -29,8 +31,6 @@ switch (actionCommand) {
         break;
 }
 
-
-
 function spotifyThis() {
     console.log("spotify")
     spotify.search({ type: 'track', query: commandResult }, function (err, data) {
@@ -38,12 +38,80 @@ function spotifyThis() {
             return console.log('Error occurred: ' + err);
         }
         console.log(data.tracks.items[0].name);
-        // console.log(data.tracks.items[0].album.name);
-        // console.log(data.tracks.items[0].artist[0].name);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].album.artists[0].name);
         // console.log(data.tracks.items[0].preview_url);  
     });
 
 }
+
+  // axios.get('http://img.omdbapi.com/?apikey=trilogy&t=up')
+ function movieThis(){
+  var queryUrl = "http://www.omdbapi.com/?t=" + commandResult + "&y=&plot=short&apikey=trilogy";
+  axios.get(queryUrl)
+  .then(function (response) {
+    // handle success
+    // * Title of the movie.
+    // * Year the movie came out.
+    // * IMDB Rating of the movie.
+    // * Rotten Tomatoes Rating of the movie.
+    // * Country where the movie was produced.
+    // * Language of the movie.
+    // * Plot of the movie.
+    // * Actors in the movie.
+ 
+    console.log("Title: " + response.data.Title);
+    console.log("Year: " +response.data.Year);
+    console.log("MPAA Rating: " +response.data.Rated);
+    console.log("Actors: " +response.data.Actors);
+    console.log(response.data.Plot);
+    console.log(response.data.Language);
+    console.log(response.data.Ratings[1]);
+    console.log(response.data.imdbRating);
+
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+ }
+
+ function concertThis(){
+  // var queryUrl = "https://rest.bandsintown.com/artists/chris%20brown/events?app_id=codingbootcamp"
+  console.log(encodeURIComponent(commandResult))
+  var queryUrl = "https://rest.bandsintown.com/artists/" + encodeURIComponent(commandResult) + "/events?app_id=codingbootcamp"
+  axios.get(queryUrl)
+   .then(function (response) {
+     // handle success
+     for (let i = 0; i < response.data.length; i++) {
+       const show = response.data[i];
+       console.log(show.venue.name)
+       console.log(show.venue.country)
+      //  show.datetime
+
+     }
+    
+   })
+   .catch(function (error) {
+     // handle error
+     console.log(error);
+   })
+   .finally(function () {
+     // always executed
+   });
+ }
+ 
+
+
+
+ 
+  
+
+
 
 
 
